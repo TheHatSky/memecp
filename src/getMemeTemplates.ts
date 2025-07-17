a0cd5f2b866fc51e5a08616cc05c09361f6a12e5
Artem Sakhatskiy <artem.sakhatskiy@proton.me>
2025-07-17 11:07:36 +0200

    feat: prefetch meme templates

---
+++ src/getMemeTemplates.ts
@@ -0,0 +1,23 @@
+type MemeTemplate = {
+  box_count: number;
+  captions: number;
+  height: number;
+  id: string;
+  name: string;
+  url: string;
+  width: number;
+};
+
+let memeTemplates: Array<MemeTemplate> = [];
+
+export const getMemeTemplates = async () => {
+  if (memeTemplates.length !== 0) return memeTemplates;
+
+  const response = await fetch("https://api.imgflip.com/get_memes");
+  const data = await response.json();
+  memeTemplates = data.data.memes.slice(0, 10);
+
+  console.log("Meme templates fetched");
+
+  return memeTemplates;
+};
